4b0f093262ee305543bd5232823a0435943debb0
JumaniyozovIbodulla <jumaniyozovibodulla07@gmail.com>
2024-05-30 11:09:12 +0500

    removed unneeded things

--- migrations/000001_user_service.up.sql
+++ migrations/000001_user_service.up.sql
@@ -1,28 +1,28 @@
 DO $$ 
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'language_enum') THEN
-    CREATE TYPE language_enum AS ENUM ('uz', 'ru', 'en');
+    CREATE TYPE "language_enum" AS ENUM ('uz', 'ru', 'en');
   END IF;
 END$$;
 
 DO $$ 
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'gender_enum') THEN
-    CREATE TYPE gender_enum AS ENUM ('Male', 'Female', 'Other');
+    CREATE TYPE "gender_enum" AS ENUM ('Male', 'Female', 'Other');
   END IF;
 END$$;
 
 DO $$ 
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'role_enum') THEN
-    CREATE TYPE role_enum AS ENUM ('admin', 'courier');
+    CREATE TYPE "role_enum" AS ENUM ('admin', 'courier');
   END IF;
 END$$;
 
 DO $$ 
 BEGIN
   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'currency_enum') THEN
-    CREATE TYPE currency_enum AS ENUM ('usdt', 'uzs');
+    CREATE TYPE currency_enum AS ENUM ('usd', 'uzs');
   END IF;
 END$$;
 
